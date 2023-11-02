41e7e8390853ecb25a0fcf75071a949cc6653515
manrivar <manrivargueta@gmail.com>
2023-11-02 15:01:34 -0600

    update

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    # ex: /pollls/
+    path("", views.index, name="index"),
+    # ex: /polls/<num>/
+    path("<int:question_id>", views.detail , name="results"),
+    # ex: /polls/<num>/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/<num>/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
