461f13784afac5cf7f2c66c71077ef44bf2cbca2
manrivar <manrivargueta@gmail.com>
2023-11-02 22:00:04 -0600

    update2

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /pollls/
     path("", views.index, name="index"),
     # ex: /polls/<num>/
-    path("<int:question_id>", views.detail , name="results"),
+    path("<int:question_id>/", views.detail , name="detail"),
     # ex: /polls/<num>/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/<num>/vote/
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 
+from django.urls import reverse
 
 # Create your views here.
 
@@ -15,11 +16,25 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "yo didn't select a choice",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results",  args=(question.id,)))
 
 
